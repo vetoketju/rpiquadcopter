8178ce667c112cbff64383b5b2aae4e9634066c1
vetoketju <ville.valta@gmail.com>
2014-08-13 19:16:25 +0300

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <thread>
 #include "udp_server.h"
+#include "controlpackage.cpp"
 
 using namespace std;
-
+private controlpackage;
 
 void listen_udp(udp_server serveri){
-  cout << "startging listener thread" << endl;
+  cout << "starting UDP-listener thread" << endl;
   serveri.start_server();
 }
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, asd asd world!" << std::endl;
+    std::cout << "RPIQuadcopter version 1.0" << std::endl;
+    
     udp_server servu(23456);
     thread udp_listener_thread(listen_udp,servu);
-    
+
+    // just a test
     for(;;){
-      
       std::this_thread::sleep_for(std::chrono::seconds(3));
       cout << "slept three sec" << endl;
     }
-    
+
+
 }
