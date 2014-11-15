27670cea1cdd572ef982f01188c8e955f9d4be5f
vetoketju <ville.valta@gmail.com>
2014-11-15 12:53:42 +0200

    Cleaning and timestamp is now u_long

--- controlpackage.cpp
+++ controlpackage.cpp
@@ -1,16 +1,13 @@
 #ifndef CONTROLPACKAGE
 #define CONTROLPACKAGE
 struct controlpackage{
-  char state;
-  //ei komentoa = 0, laita paalle = 1, sammuta = 2, kamera paalle = 3,  kamera pois = 4,
+  char command; // Command
   char axis_x; // forwards - backwards
   char axis_y; // up -down
   char axis_z; // left - right
   char axis_r; // Rotation
   char cam_x;  // Pan
   char cam_y;  // Tilt
-  //kummallekin akselille yksi byte, 0: paikallaan, 1 ja 2 eri suunnat
-  //Optimoi myöh. yhteen byteen
-  //LISÄÄ TIMESTAMP
+  unsigned long timestamp;
 };
 #endif
